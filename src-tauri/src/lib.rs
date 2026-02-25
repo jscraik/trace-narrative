50ceea4fcc206758060117998aadd3067e376d4d
jscraik <154467285+jscraik@users.noreply.github.com>
2026-02-25 10:55:51 +0000

    fix(tauri): allow firefly settings store plugin in ACL

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -327,6 +327,7 @@ pub fn run() -> Result<(), Box<dyn std::error::Error>> {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(
             tauri_plugin_sql::Builder::default()
